be03731713fefa179ecf0f8d61d1369d8c1ed5ce
UlugbekTemirov <temirovulugbek2003@gmail.com>
2022-11-12 12:36:18 +0500

    `youtube link and current game added`

--- src/components/CurrentGame.js
+++ src/components/CurrentGame.js
@@ -1,25 +1,50 @@
 import * as React from "react";
+import { domainName } from "../global/global";
 
 import "./CurrentGame.css";
-// import zakovat from "../images/zakovatImg.jpg";
+import zakovat from "../images/zakovat.png";
 
-const CurrentGame = () => {
+const CurrentGame = (props) => {
+  const [data, setData] = React.useState([]);
+
+  const fetchData = () => {
+    fetch(`${domainName}/api/v1/current-game/`)
+      .then((promise) => promise.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  setTimeout(fetchData, 30000);
+
+  if (data.team1 == "")
+    return (
+      <h1 className="text-danger text-opacity-75 text-center mt-5">
+        Hozirda mavjud o'yinlar topilmadi
+      </h1>
+    );
+
   return (
     <div className="row current-game">
       <div className="col-md-6 col-sm-12 first-team teams mt-3">
-        <img className="backgroundImg" src="" alt="something" />
+        <img className="backgroundImg" src={zakovat} alt="something" />
         <div className="results">
-          <div className="group-name text-white">O'zbekona</div>
+          <div className="group-name text-white">{data.team1}</div>
           <span className="divider"></span>
-          <div className="group-score text-white">0</div>
+          <div className="group-score text-white">{data.score1}</div>
         </div>
       </div>
       <div className="col-md-6 col-sm-12 second-team teams mt-3">
-        <img className="backgroundImg" src="" alt="something" />
+        <img className="backgroundImg" src={zakovat} alt="something" />
         <div className="results">
-          <div className="group-name text-white">Major</div>
+          <div className="group-name text-white">{data.team2}</div>
           <span className="divider"></span>
-          <div className="group-score text-white">0</div>
+          <div className="group-score text-white">{data.score2}</div>
         </div>
       </div>
     </div>
--- src/pages/LiveShare.js
+++ src/pages/LiveShare.js
@@ -1,21 +1,37 @@
 import * as React from "react";
 import CurrentGame from "../components/CurrentGame";
+import { domainName } from "../global/global";
 
 const LiveShare = () => {
+  const [data, setData] = React.useState([]);
+  React.useState(() => {
+    fetch(`${domainName}/api/v1/youtube-link/`)
+      .then((promise) => promise.json())
+      .then((data) => setData(data));
+  }, []);
+
+  if (data.link === "")
+    return (
+      <h1 className="text-danger text-center text-opacity-75 mt-5">
+        Hozirda mavjud o'yinlar topilmadi
+      </h1>
+    );
+
   return (
     <>
+      <h1 className="text-center mb-3 mt-5">{data.description}</h1>
       <div className="d-flex justify-content-center mt-5">
         <iframe
           width="100%"
           height="700"
-          src="https://www.youtube.com/embed/04rO9kKruTM"
+          src={data.link}
           title="Zakovat o'yinlari"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         />
       </div>
-      {/* <CurrentGame /> */}
+      <CurrentGame />
     </>
   );
 };
@@ -23,5 +39,5 @@ const LiveShare = () => {
 export default LiveShare;
 
 {
-  /* <iframe width="560" height="315" src="https://www.youtube.com/embed/04rO9kKruTM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */
+  /* <iframe width="560" height="315" src="https://www.youtube.com/embed/jfKfPfyJRdk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */
 }
